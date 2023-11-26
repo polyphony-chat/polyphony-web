45bda4912d8600c269b7d5c3b8548cd26e4b91b1
bitfl0wer <florian@pro-weber.com>
2023-11-26 23:18:30 +0100

    Create AccountWrapper and related structs + state with yewdux

--- crates/polyphony-wasm/src/app/dashboard/mod.rs
+++ crates/polyphony-wasm/src/app/dashboard/mod.rs
@@ -1,11 +1,28 @@
 use yew::prelude::*;
 
-#[function_component(Dashboard)]
-pub fn dashboard() -> Html {
-    html! {
-        <div class="dash">
-            <h1>{"Dashboard"}</h1>
-            <p>{":)"}</p>
-        </div>
+#[derive(Clone, Default, PartialEq, Properties)]
+pub struct Dashboard;
+
+#[derive(Properties, PartialEq)]
+pub struct DashboardProps;
+
+pub enum DashboardMsg {}
+
+impl Component for Dashboard {
+    type Message = DashboardMsg;
+
+    type Properties = DashboardProps;
+
+    fn create(_ctx: &Context<Self>) -> Self {
+        todo!()
+    }
+
+    fn view(&self, _ctx: &Context<Self>) -> Html {
+        html! {
+            <div class="dash">
+                <h1>{"Dashboard"}</h1>
+                <p>{":)"}</p>
+            </div>
+        }
     }
 }
--- crates/polyphony-wasm/src/app/mod.rs
+++ crates/polyphony-wasm/src/app/mod.rs
@@ -3,8 +3,13 @@ mod info;
 mod login;
 mod register;
 
+use chorus::instance::ChorusUser;
+use hashbrown::HashMap;
 use yew::prelude::*;
 use yew_router::prelude::*;
+use yewdux::prelude::*;
+
+use crate::GlobalIdentifier;
 
 use self::dashboard::Dashboard;
 use self::info::Info;
@@ -19,6 +24,40 @@ pub fn App() -> Html {
             <Switch<Route> render={switch} />
         </BrowserRouter>
     }
+}
+
+/// Client-relevant data about the current user.
+#[derive(Clone, Default)]
+pub struct AccountWrapper {
+    /// The current user.
+    pub user: Option<ChorusUser>,
+    /// Whether the user is logged in, i.e. has a valid session.
+    pub active: bool,
+    /// The user's global identifier.
+    pub global_identifier: GlobalIdentifier,
+}
+
+impl PartialEq for AccountWrapper {
+    fn eq(&self, other: &Self) -> bool {
+        self.active == other.active && self.global_identifier == other.global_identifier
+    }
+}
+
+#[derive(Clone, Default, PartialEq)]
+pub struct ClientSettings {
+    language: String,
+}
+
+#[derive(PartialEq, Store, Default)]
+pub struct State {
+    dashboard: Dashboard,
+}
+
+#[derive(PartialEq, Store, Default)]
+#[store(storage = "local", storage_tab_sync)]
+pub struct TabSyncedState {
+    client_settings: ClientSettings,
+    users: HashMap<GlobalIdentifier, AccountWrapper>,
 }
 
 #[derive(Clone, Routable, PartialEq)]
