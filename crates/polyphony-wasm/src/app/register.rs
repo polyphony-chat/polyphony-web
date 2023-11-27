ef9a5e5f372b23b8b78c1079051062dabc3a3784
bitfl0wer <florian@pro-weber.com>
2023-11-27 21:21:36 +0100

    Add registration form to the app

--- crates/polyphony-wasm/src/app/register.rs
+++ crates/polyphony-wasm/src/app/register.rs
@@ -1,11 +1,23 @@
 use yew::prelude::*;
 
+struct RegisterProps {}
+
 #[function_component(Register)]
 pub fn register() -> Html {
     html! {
-        <div class="register">
+        <form class="register">
             <h1>{"Register"}</h1>
-            <p>{"Sign up"}</p>
-        </div>
+            <label for ="username">{"Username"}</label>
+            <input type="text" id="username" required=true/>
+            <label for ="password">{"Password"}</label>
+            <input type="password" id="password" required=true/>
+            <label for ="email">{"Email"}</label>
+            <input type="email" id="email"/>
+            <label for="consent">{"I agree to the terms of service."}</label>
+            <input type="checkbox" id="consent" required=true/>
+            <label for="age">{"I am at least 16 years old."}</label>
+            <input type="checkbox" id="age" required=true/>
+            <input type="submit" value="Sign Up" required=true/>
+        </form>
     }
 }
