0cf19abc057167ed808d0585ea51e01fdbf338b6
bitfl0wer <florian@pro-weber.com>
2023-11-28 11:51:53 +0100

    yewdux experiments

--- crates/polyphony-wasm/src/app/register.rs
+++ crates/polyphony-wasm/src/app/register.rs
@@ -1,14 +1,23 @@
 use yew::prelude::*;
+use yewdux::prelude::*;
 
-struct RegisterProps {}
+#[derive(Store, PartialEq, Clone, Default)]
+struct RegisterProps {
+    username: String,
+    password: String,
+    email: String,
+    consent: bool,
+    age: bool,
+}
 
 #[function_component(Register)]
 pub fn register() -> Html {
+    let (props, dispatch) = use_store::<RegisterProps>();
     html! {
         <form class="register">
             <h1>{"Register"}</h1>
             <label for ="username">{"Username"}</label>
-            <input type="text" id="username" required=true/>
+            <input type="text" id="username" required=true oninput={move |input: InputEvent| dispatch.clone().reduce_mut(|props| props.username = input.as_string().unwrap()) } />
             <label for ="password">{"Password"}</label>
             <input type="password" id="password" required=true/>
             <label for ="email">{"Email"}</label>
@@ -18,6 +27,7 @@ pub fn register() -> Html {
             <label for="age">{"I am at least 16 years old."}</label>
             <input type="checkbox" id="age" required=true/>
             <input type="submit" value="Sign Up" required=true/>
+            <p>{props.username.clone()}</p>
         </form>
     }
 }
