f6dfa67376fd8c08cb60b44c9a3221d0f69ddd87
bitfl0wer <florian@pro-weber.com>
2023-11-28 15:44:30 +0100

    Almost working form :)

--- crates/polyphony-wasm/src/app/register.rs
+++ crates/polyphony-wasm/src/app/register.rs
@@ -1,3 +1,4 @@
+use web_sys::HtmlInputElement;
 use yew::prelude::*;
 use yewdux::prelude::*;
 
@@ -13,21 +14,45 @@ struct RegisterProps {
 #[function_component(Register)]
 pub fn register() -> Html {
     let (props, dispatch) = use_store::<RegisterProps>();
+    let onchange_username = {
+        let dispatch_clone = dispatch.clone();
+        move |value: Event| {
+            dispatch_clone.reduce_mut(|state| {
+                state.username = value.target_unchecked_into::<HtmlInputElement>().value()
+            })
+        }
+    };
+    let onchange_password = {
+        let dispatch_clone = dispatch.clone();
+        move |value: Event| {
+            dispatch_clone.reduce_mut(|state| {
+                state.password = value.target_unchecked_into::<HtmlInputElement>().value()
+            })
+        }
+    };
+    let onchange_email = {
+        let dispatch_clone = dispatch.clone();
+        move |value: Event| {
+            dispatch_clone.reduce_mut(|state| {
+                state.email = value.target_unchecked_into::<HtmlInputElement>().value()
+            })
+        }
+    };
     html! {
         <form class="register">
             <h1>{"Register"}</h1>
             <label for ="username">{"Username"}</label>
-            <input type="text" id="username" required=true oninput={move |input: InputEvent| dispatch.clone().reduce_mut(|props| props.username = input.as_string().unwrap()) } />
+            <input type="text" id="username" required=true onchange={onchange_username} />
             <label for ="password">{"Password"}</label>
-            <input type="password" id="password" required=true/>
+            <input type="password" id="password" required=true onchange={onchange_password}/>
             <label for ="email">{"Email"}</label>
-            <input type="email" id="email"/>
+            <input type="email" id="email" onchange={onchange_email}/>
             <label for="consent">{"I agree to the terms of service."}</label>
             <input type="checkbox" id="consent" required=true/>
-            <label for="age">{"I am at least 16 years old."}</label>
+            <label for="age">{"I am at least 16 years old."} </label>
             <input type="checkbox" id="age" required=true/>
             <input type="submit" value="Sign Up" required=true/>
-            <p>{props.username.clone()}</p>
+            <p>{props.age}</p>
         </form>
     }
 }
