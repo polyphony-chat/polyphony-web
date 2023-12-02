c3cb1495e6c6c2d11aa339ae1d334128c3c9ac08
bitfl0wer <florian@pro-weber.com>
2023-12-02 21:11:31 +0100

    Add UserStore, modify AuthStore

--- crates/polyphony-wasm/src/stores.rs
+++ crates/polyphony-wasm/src/stores.rs
@@ -1,9 +1,18 @@
-use chorus::instance::Instance;
+use chorus::instance::{ChorusUser, Instance};
 use chorus::UrlBundle;
 use serde::{Deserialize, Serialize};
 use yewdux::prelude::*;
 
+use crate::GlobalIdentifier;
+
 #[derive(Store, Clone, PartialEq, Debug, Default, Deserialize, Serialize)]
 pub(crate) struct AuthenticationStore {
-    instances: Vec<(UrlBundle, Instance)>,
+    pub(crate) instances: Vec<(UrlBundle, Instance)>,
+    /// Tuple (GlobalIdentifier, Token)
+    pub(crate) identities: Vec<(GlobalIdentifier, String)>,
+}
+
+#[derive(Store, Default, PartialEq)]
+pub(crate) struct UserStore {
+    users: Vec<(GlobalIdentifier, ChorusUser)>,
 }
