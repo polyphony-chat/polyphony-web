f2cdcc52776b4db7edb007cf2b45c19a13f7f30d
bitfl0wer <florian@pro-weber.com>
2023-12-09 17:38:00 +0100

    Clean slate

--- crates/polyphony-wasm/src/app/register.rs
+++ crates/polyphony-wasm/src/app/register.rs
@@ -1,15 +1,1 @@
-use dioxus::prelude::*;
 
-#[derive(Props, PartialEq)]
-pub struct RegisterProps<'a> {
-    url_api: &'a str,
-}
-
-#[allow(non_snake_case)]
-pub fn Register<'a>(cx: Scope<'a, RegisterProps<'a>>) -> Element {
-    cx.render(rsx!(
-        p {
-            "register {cx.props.url_api}"
-        }
-    ))
-}
--- crates/polyphony-wasm/src/main.rs
+++ crates/polyphony-wasm/src/main.rs
@@ -2,28 +2,11 @@ mod app;
 
 use chorus::types::Snowflake;
 use chorus::UrlBundle;
-use dioxus::prelude::*;
-use dioxus_router::prelude::*;
-
-use crate::app::Register;
 
 fn main() {
-    dioxus_web::launch(App);
+    panic!()
 }
 
 /// Tuple of a [`UrlBundle`] and a [`Snowflake`], where the [`Snowflake`] is the ID of the User and
 /// the [`UrlBundle`] are the URLs of the Instance.
 pub type GlobalIdentifier = (UrlBundle, Snowflake);
-
-#[derive(Routable, Clone)]
-enum Route {
-    #[route("/register")]
-    Register { url_api: &'static str },
-}
-
-#[allow(non_snake_case)]
-fn App(cx: Scope) -> Element {
-    render! {
-        Router::<Route> {}
-    }
-}
