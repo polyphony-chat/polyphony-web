7da34fb528e24fee5aec1301684d5b5a9b92b168
bitfl0wer <florian@pro-weber.com>
2023-12-11 18:59:50 +0100

    Registering works!

--- crates/polyphony-wasm/src/app/register.rs
+++ crates/polyphony-wasm/src/app/register.rs
@@ -1,12 +1,10 @@
 use chorus::errors::ChorusResult;
-use chorus::instance::Instance;
+use chorus::instance::{ChorusUser, Instance};
 use chorus::types::RegisterSchema;
 use chorus::UrlBundle;
 use hashbrown::HashMap;
 use leptos::*;
 use log::*;
-
-use crate::stores::InstanceStore;
 
 #[component]
 pub fn Register() -> impl IntoView {
@@ -42,30 +40,44 @@ pub fn Register() -> impl IntoView {
     }
 }
 
-async fn register(input: &(String, String, String)) -> ChorusResult<Instance> {
-    let reg = RegisterSchema {
+async fn register(input: &(String, String, String)) -> ChorusResult<ChorusUser> {
+    let register_schema = RegisterSchema {
         username: input.2.clone(),
         password: Some(input.1.clone()),
         consent: true,
         email: Some(input.2.clone()),
+        date_of_birth: Some("2000-01-01".to_string()),
         ..Default::default()
     };
     let instance_store = use_context::<RwSignal<HashMap<UrlBundle, Instance>>>().unwrap();
     let urls = UrlBundle::from_root_url(&input.0).await?;
-    if let Some(instance) = instance_store.get_untracked().get(&urls) {
-        debug!("Got cached instance: {:#?}", instance);
-        Ok(instance.clone())
-    } else {
-        debug!("No cached instance found. Attempting lookup");
-        debug!("Attempting to create instance from URLBundle {:#?}", &urls);
-        let new_instance = Instance::new(urls.clone()).await.unwrap();
-        debug!(
-            "Got fresh instance: {:#?}; Inserting into cache",
-            &new_instance
-        );
-        instance_store.update(|map| {
-            map.insert(urls, new_instance.clone());
-        });
-        Ok(new_instance)
-    }
+    let mut instance = match instance_store.get_untracked().get(&urls) {
+        Some(cached_instance) => {
+            debug!("Got cached instance: {:#?}", cached_instance);
+            cached_instance.clone()
+        }
+        None => {
+            debug!("No cached instance found. Attempting lookup");
+            debug!("Attempting to create instance from URLBundle {:#?}", &urls);
+            let new_instance = Instance::new(urls.clone()).await.unwrap();
+            debug!(
+                "Got fresh instance: {:#?}; Inserting into cache",
+                &new_instance
+            );
+            instance_store.update(|map| {
+                map.insert(urls.clone(), new_instance.clone());
+            });
+            new_instance
+        }
+    };
+    debug!(
+        "Registering account on instance {}",
+        instance.instance_info.instance_name
+    );
+    let account = instance.register_account(register_schema).await;
+    instance_store.update(|map| {
+        map.insert(urls, instance);
+    });
+    debug!("Got account with token {}", account.as_ref().unwrap().token);
+    account
 }
